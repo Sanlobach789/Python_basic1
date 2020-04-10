aee2048872db4261bd3c573ebc63c806817d847c
sanlobach <sanlobach789@gmail.com>
2020-04-10 11:52:18 +0300

    start les1 ex4

--- les1_exe4.py
+++ les1_exe4.py
@@ -3,6 +3,6 @@ max_numb = int(some_numb) % 10
 some_numb = int(some_numb) // 10
 while some_numb > 0:
     if some_numb % 10 > max_numb:
-        max_numb = some_numb
+        max_numb = some_numb % 10
     some_numb = some_numb // 10
 print(max_numb)
