c990c43e6128af8f9a9db467547bcd93ceda985e
sanlobach <sanlobach789@gmail.com>
2020-04-12 12:00:07 +0300

    Урок2 задание 3

--- les2/les2_ex2.py
+++ les2/les2_ex2.py
@@ -3,7 +3,7 @@ print(usr_list)
 print(len(usr_list))
 i = 0
 
-if (len(usr_list)-1) % 2 == 0:
+if (len(usr_list) - 1) % 2 == 0:
     while i < len(usr_list) - 1:
         a = usr_list[i]
         b = usr_list[i + 1]
