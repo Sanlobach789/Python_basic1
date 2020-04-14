7002c8a7eb115eddb1e59cb685f106eb802a5624
sanlobach <sanlobach789@gmail.com>
2020-04-14 19:11:27 +0300

    Урок2 задание 6 первый вариант

--- les2/les2_ex6.py
+++ les2/les2_ex6.py
@@ -1,21 +1,32 @@
-from typing import List
-
-good_id = 0
-total_list = {}
-while good_id <= 2:
-    item = input("Введите характеристики товара через запятую 'название цена количество единица измерения': ")
-    good_char = list(item.split(","))
-    if len(good_char) > 4:
-        print("Вы ввели больше характеристик.")
-    elif len(good_char) < 4:
-        print("Вы ввели не все характеристики")
-    elif len(good_char) == 4:
-        good_id += 1
-        goods = {"": good_id,
-                 "Название ": str(good_char[0]),
-                 "Цена ": str(good_char[1]),
-                 "Количество ": str(good_char[2]),
-                 "ед.": str(good_char[3])}
-        print(goods)
-        total_list = dict(list(total_list.items()) + list(goods.items()))
-print(total_list)
+i = 0
+goods_list = []
+names = []
+prices = []
+counts = []
+units = []
+while i <= 2:
+    item_id = input("номер товара: ")
+    item_id = int(item_id)
+    item_name = input("Введите название товара: ")
+    item_price = input("Введите цену товара: ")
+    item_price = int(item_price)
+    item_count = input("Введите количество товара: ")
+    item_count = int(item_count)
+    item_unit = input("Введите единицу измерения: ")
+    good_info = {"Название ": item_name,
+                 "цена ": item_price,
+                 "количество ": item_count,
+                 "Единица измерения ": item_unit}
+    good = [(item_id, good_info)]
+    goods_list.extend(good)
+    names.append(item_name)
+    prices.append(item_price)
+    counts.append(item_count)
+    if item_unit not in units:
+        units.append(item_unit)
+    i += 1
+goods_analytics = {"Название: ": names,
+                   "цена: ": prices,
+                   "количество: ": counts,
+                   "ед: ": units}
+print(goods_analytics)
