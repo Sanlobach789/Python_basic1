dfe1b0a1ebe6fbe9aeacd00b4b7c94ea9d13003b
sanlobach <sanlobach789@gmail.com>
2020-04-14 19:21:43 +0300

    Урок2 задание 4 исправил ошибку

--- les2/les2_ex4.py
+++ les2/les2_ex4.py
@@ -2,5 +2,5 @@ text = input("Введите фразу: ")
 text_mas = list(text.split())
 i = 0
 while i <= len(text_mas) - 1:
-    print("\n" + str(i + 1) + ": " + str(text_mas[i]))
+    print("\n" + str(i + 1) + ": " + str(text_mas[i][:10]))
     i += 1
