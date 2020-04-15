453c39a996066166ed589802af9e8feefd2a54a5
sanlobach <sanlobach789@gmail.com>
2020-04-15 20:33:44 +0300

    Урок2 задание 6 финальный вариант) наверное

--- les2/les2_ex6.py
+++ les2/les2_ex6.py
@@ -1,32 +1,28 @@
 i = 0
 goods_list = []
-names = []
-prices = []
-counts = []
-units = []
-while i <= 2:
-    item_number = input("номер товара: ")
-    item_number = int(item_number)
+goods_analytics = {"Название": list(),
+                   "Цена": list(),
+                   "Количество": list(),
+                   "Ед": list()}
+count = int(input("Кол-во добавляемого товара: "))
+while i < count:
+    item_id = int(input("номер товара: "))
     item_name = input("Введите название товара: ")
-    item_price = input("Введите цену товара: ")
-    item_price = int(item_price)
-    item_count = input("Введите количество товара: ")
-    item_count = int(item_count)
+    item_price = int(input("Введите цену товара: "))
+    item_count = int(input("Введите количество товара: "))
     item_unit = input("Введите единицу измерения: ")
-    good_info = {"Название ": item_name,
-                 "цена ": item_price,
-                 "количество ": item_count,
-                 "Единица измерения ": item_unit}
-    good = [(item_number, good_info)]
+    good_info = {"Название": item_name,
+                 "Цена": item_price,
+                 "Количество": item_count,
+                 "Ед": item_unit}
+    good = [(item_id, good_info)]
     goods_list.extend(good)
-    names.append(item_name)
-    prices.append(item_price)
-    counts.append(item_count)
-    if item_unit not in units:
-        units.append(item_unit)
     i += 1
-goods_analytics = {"Название: ": names,
-                   "цена: ": prices,
-                   "количество: ": counts,
-                   "ед: ": units}
+
+for item in goods_list:
+    goods_analytics["Название"].append(item[1].get('Название'))
+    goods_analytics["Цена"].append(item[1].get('Цена'))
+    goods_analytics["Количество"].append(item[1].get('Количество'))
+    goods_analytics["Ед"].append(item[1].get('Ед'))
+
 print(goods_analytics)
